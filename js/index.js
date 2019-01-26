de37f406a6c5cc3f8f128b1e416eb0f85dfd0a79
Morgas <morgas01@web.de>
2019-01-26 18:13:23 +0100

    use OrganizedTable

--- js/index.js
+++ js/index.js
@@ -3,7 +3,11 @@
 	let rq=GMOD("request");
 
 	SC=SC({
-		SortTable:"SortTable"
+		Table:"gui.OrganizedTable",
+		ORG:"Organizer",
+		Config:"Config",
+		form:"gui.form",
+		remove:"array.remove"
 	});
 
 	let parseDuration=function(duration)
@@ -25,53 +29,150 @@
 	rq.json("rest/list/data")
 	.then(function(data)
 	{
-		let table=new SC.SortTable();
+		let table=new SC.Table();
 		let tableConfig=table.tableConfig;
 		tableConfig.addColumn({
 			name:"name",
 			styleClass:"anime_title",
-			fn:(cell,data)=>cell.innerHTML=`<img src="${data.anime_image_path}"><a href="https://myanimelist.net${data.anime_url}">${data.anime_title}</a>`,
-			sortValue:data=>data.anime_title.toLowerCase()
+			fn:(cell,data)=>
+			{
+				cell.innerHTML=`<!--img src="${data.anime_image_path}"--><a href="https://myanimelist.net${data.anime_url}">${data.anime_title}</a>`;
+				cell.dataset.column="anime_title";
+			}
 		});
 		tableConfig.addColumn({
 			name:"score",
 			styleClass:"user_score",
-			fn:(cell,data)=>cell.textContent=data["user_score"],
-			sortValue:data=>data.user_score
+			fn:(cell,data)=>
+			{
+				cell.textContent=data.user_score;
+				cell.dataset.column="user_score";
+			}
 		});
 		tableConfig.addColumn({
 			name:"genres",
 			styleClass:"genres",
-			fn:(cell,data)=>cell.textContent=data["genres"],
-			sortValue:data=>data.genres.length
+			fn:(cell,data)=>
+			{
+				cell.textContent=data.genres;
+			}
+
 		});
 		tableConfig.addColumn({
 			name:"episodes",
 			styleClass:"anime_num_episodes",
-			fn:(cell,data)=>cell.textContent=data["anime_num_episodes"],
-			sortValue:data=>data.anime_num_episodes
+			fn:(cell,data)=>
+			{
+				cell.textContent=data["anime_num_episodes"];
+				cell.dataset.column="anime_num_episodes";
+			}
 		});
 		tableConfig.addColumn({
 			name:"duration",
 			styleClass:"duration",
-			fn:(cell,data)=>cell.textContent=data["duration"],
-			sortValue:data=>parseDuration(data.duration)
+			fn:(cell,data)=>
+			{
+				cell.textContent=data["duration"];
+				cell.dataset.column="duration";
+			}
 		});
 		tableConfig.addColumn({
 			name:"total duration",
 			styleClass:"total_duration",
 			fn:(cell,data)=>
 			{
-				let totalDuration=parseDuration(data["duration"])*data.anime_num_episodes;
+				let totalDuration=parseDuration(data.duration)*data.anime_num_episodes;
 				cell.textContent=getTimeString(totalDuration);
-				cell.title=totalDuration||data.duration;
-			},
-			sortValue:data=>parseDuration(data["duration"])*data.anime_num_episodes
+				cell.dataset.column="total_duration";
+			}
 		});
 
-		table.add(data);
+		table.addGroup("genres","genres").add(data);
+
+		requestAnimationFrame(()=>requestAnimationFrame(()=>
+		{
+			let start=performance.now();
+			table.addSort("title",SC.ORG.attributeSort("anime_title"))
+			.addSort("score",SC.ORG.attributeSort("user_score"))
+			.addSort("episodes",SC.ORG.attributeSort("anime_num_episodes"))
+			.addSort("duration",SC.ORG.orderBy(data=>parseDuration(data.duration)))
+			.addSort("total duration",SC.ORG.orderBy(data=>parseDuration(data.duration)*data.anime_num_episodes));
+			console.log("sort time: "+(performance.now()-start));
+		}));
+
+		let searchConfig=new SC.Config.parse({
+			sort:/*[*/
+				{
+					column:{
+						type:"select",
+						values:["","title","score","episodes","duration","total Duration"]
+					},
+					direction:{
+						type:"select",
+						values:["ascending","descending"],
+						default:"ascending"
+					}
+				}
+			/*]*/,
+			filter:{
+				genres:{
+					...table.getGroupParts("genres").reduce((obj,genre)=>(obj[genre]=false,obj),{})
+				}
+			}
+		});
+
+		let form=SC.form(searchConfig);
+		document.querySelector("#control").appendChild(form);
 
 		document.body.appendChild(table.getTable());
-	});
+
+		form.addEventListener("formChange",function(event)
+		{
+			let path=event.detail.path.slice();
+			let pathStep=path.shift();
+			switch(pathStep)
+			{
+				case "sort":
+					switch(event.detail.key)
+					{
+						case "column":
+							table.setSort(event.detail.value,table.getSortReverse());
+							break;
+						case "direction":
+							table.setSort(table.getSort(),event.detail.value);
+							break;
+					}
+					break;
+				case "filter":
+					pathStep=path.shift();
+					switch(pathStep)
+					{
+						case "genres":
+							let groupParts=table.getGroups()["genres"]||[];
+							if(event.detail.value) groupParts.push(event.detail.key);
+							else SC.remove(groupParts,event.detail.key);
+							table.setGroup("genres",groupParts);
+							break;
+						default:
+							return;
+					}
+					break;
+				default:
+					return;
+			}
+			table.updateTable();
+		})
+/*
+		table.tableHeader.addEventListener("click",function(event)
+		{
+			if(event.target.tagName==="TH")
+			{
+				let column=event.target.classList.value;
+				if(table.getSort()===column) table.setSort(null);
+				else table.setSort(column);
+				table.updateTable();
+			}
+		});*/
+	},alert);
 
 })(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
